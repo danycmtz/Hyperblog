6a539b5c0c739b8e002c917b926de497f58e2c82
Dany Corral <danycorral17@gmail.com>
2021-02-01 21:55:13 -0600

    Arranco un proyecto con python

---
+++ Experimento.py
@@ -0,0 +1,7 @@
+print("Esta es una prueba de git")
+
+print("Freddy está haciendo una página de HTML y yo estoy practicando python")
+a = 5
+b = 10
+c = a + b
+print("Hice una suma cuyo resultado es: ",c)
